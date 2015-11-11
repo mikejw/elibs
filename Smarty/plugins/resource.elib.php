ae5bc4e5d24e45774faed653beca0c478982af3b
Mike Whiting <mail@mikejw.co.uk>
2015-11-11 18:15:52 +0000

    fixes.

--- Smarty/plugins/resource.elib.php
+++ Smarty/plugins/resource.elib.php
@@ -49,14 +49,14 @@ function smarty_resource_elib_timestamp($tpl_name, &$tpl_timestamp, $smarty)
     if (!file_exists($filename)) {
         if (isset($smarty->tpl_vars['elibtpl_arr'])) {
 
-            foreach ($smarty->tpl_vars->value['elibtpl_arr'] as $dir) {
+            foreach ($smarty->tpl_vars['elibtpl_arr']->value as $dir) {
                 $filename = $dir.'/'.$tpl_name;
                 if (file_exists($filename)) {
                     break;
                 }
             }
         } else {
-            $filename = $smarty->tpl_vars->value['elibtpl'].$tpl_name;
+            $filename = $smarty->tpl_vars['elibtpl']->value.$tpl_name;
         }
     }
 
